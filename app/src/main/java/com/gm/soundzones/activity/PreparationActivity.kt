5eec02f702c1ee0a6ee6d82b998c7a9d08bb523b
Pavel Aizendorf <titan18gmail.com>
2017-10-19 17:02:53 +0300

    texts update

--- app/src/main/java/com/gm/soundzones/activity/PreparationActivity.kt
+++ app/src/main/java/com/gm/soundzones/activity/PreparationActivity.kt
@@ -9,6 +9,7 @@ import android.view.Gravity
 import android.view.View
 import com.gm.soundzones.R
 import com.gm.soundzones.excel.DataProvider
+import com.gm.soundzones.fragment.ExplanationFragment
 import com.gm.soundzones.fragment.InformationFragment
 import com.gm.soundzones.listener.OnClickNextListener
 import com.gm.soundzones.loader.HtmlTagHandler
@@ -24,14 +25,7 @@ class PreparationActivity : AppCompatActivity(), OnClickNextListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_toolbar_container)
         if (savedInstanceState == null) {
-            val welcomeFragment = InformationFragment.newInstance(
-                    title = getString(R.string.intro_title),
-                    desc2 = Html.fromHtml(getString(R.string.omg_html)),
-                    infoGravity = Gravity.START,
-                    desc3 = getText(R.string.intro_ready),
-                    desc4 = UserDataManager.userID.toString(),
-                    desc5 = getString(R.string.any_questions),
-                    btnVisibility = View.INVISIBLE)
+            val welcomeFragment = ExplanationFragment()
             supportFragmentManager.beginTransaction().add(R.id.container, welcomeFragment)
                     .commitNow()
 
--- app/src/main/java/com/gm/soundzones/fragment/ExplanationFragment.kt
+++ app/src/main/java/com/gm/soundzones/fragment/ExplanationFragment.kt
@@ -1,9 +1,35 @@
 package com.gm.soundzones.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.gm.soundzones.R
+import kotlinx.android.synthetic.main.explanation_layout.*
+
 /**
  * Created by Pavel Aizendorf on 19/10/2017.
  */
 class ExplanationFragment : BaseFragment() {
 
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? = inflater?.inflate(R.layout.explanation_layout, container, false)
+
+    override fun update(args: Bundle) {
+        super.update(args)
+        args.takeIf { it.containsKey(EXTRA_BTN_VISIBILITY) }?.run {
+            btnNext.visibility = getInt(EXTRA_BTN_VISIBILITY)
+        }
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btnNext.setOnClickListener {
+            onClickNext()
+        }
+    }
+
+    companion object {
+        val EXTRA_BTN_VISIBILITY = "extra_btn_visibility"
+    }
 
 }
